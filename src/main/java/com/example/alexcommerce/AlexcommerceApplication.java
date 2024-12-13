e8247a386b9c2b3233fcb2c56fc3b4f550d84fb4
Alexandre <alexandrenas2003@gmail.com>
2024-12-13 16:45:59 -0300

    first commit

---
+++ src/main/java/com/example/alexcommerce/AlexcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.alexcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexcommerceApplication.class, args);
+	}
+
+}
