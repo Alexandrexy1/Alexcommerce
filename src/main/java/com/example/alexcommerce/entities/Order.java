241527ea1f9153e95bd023116b9182e37ee7ba08
Alexandre <alexandrenas2003@gmail.com>
2024-12-13 21:16:05 -0300

    Order, User entities and OrderStatus

---
+++ src/main/java/com/example/alexcommerce/entities/Order.java
@@ -0,0 +1,51 @@
+package com.example.alexcommerce.entities;
+
+import com.example.alexcommerce.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Order() {}
+
+    public Order(Instant moment, OrderStatus status) {
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/example/alexcommerce/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.alexcommerce.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
