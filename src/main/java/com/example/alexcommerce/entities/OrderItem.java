6d6e0f6196c0933fefde898c13a6f03e7ef79945
Alexandre <alexandrenas2003@gmail.com>
2024-12-16 09:35:04 -0300

    OrderItem, OrderItemPK

---
+++ src/main/java/com/example/alexcommerce/entities/OrderItem.java
@@ -0,0 +1,58 @@
+package com.example.alexcommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+
+@Entity
+public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Double price;
+    private Integer quantity;
+
+    public OrderItem() {}
+
+    public OrderItem(Order order, Product product, Double price, Integer quantity) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public OrderItemPK getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
